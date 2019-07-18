777ad26169bed886794ac42152358ef3768e7f45
Adalberto Cubillo <acubillo@growthaccelerationpartners.com>
2019-07-18 00:03:49 -0600

    Changed the code to download the data from the UC Irvine site.

--- plot1.R
+++ plot1.R
@@ -3,25 +3,36 @@
 #  Created by Adalberto Cubillo on 7/17/19.
 #  Copyright © 2019 Adalberto Cubillo. All rights reserved.
 
+# Download the household power consumption data zipped (the file is huge to store in github!).
+temp <- tempfile()
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", temp)
+downloaded_file <- unz(temp, "household_power_consumption.txt")
+
 # Read the household power consumption data. 
-data_header <- read.table(file = "./household_power_consumption.txt", 
-                   header = FALSE, sep = ";", nrows = 1, stringsAsFactors = FALSE)
-data <- read.table(file = "./household_power_consumption.txt", 
-                   header = FALSE, sep = ";", skip = 66637, nrows = 2880)
-colnames(data) <- as.vector(data_header[1,])
+data_header <- read.table(file = downloaded_file, 
+                          header = FALSE, sep = ";", nrows = 1, stringsAsFactors = FALSE)
+
+# I need to load it again after the first read.
+downloaded_file <- unz(temp, "household_power_consumption.txt") 
+hpc_data <- read.table(file = downloaded_file,
+                       header = FALSE, sep = ";", skip = 66637, nrows = 2880)
+colnames(hpc_data) <- as.vector(data_header[1,])
+
+# Delete temp file.
+unlink(temp)
 
 # Convert Date and Time variables into R date and time types.
-data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
-data$Time <- format(strptime(data$Time, format = "%H:%M:%S"), "%H:%M:%S")
+hpc_data$Date <- as.Date(hpc_data$Date, format = "%d/%m/%Y")
+hpc_data$Time <- format(strptime(hpc_data$Time, format = "%H:%M:%S"), "%H:%M:%S")
 
 # Set the plot device to PNG and the size to 480x480px.
 png(filename = "./plot1.png", width = 480, height = 480, units = "px")
 
 # Create the plot1.
-with(data, hist(Global_active_power, 
-                col = "red", 
-                xlab = "Global Active Power (kilowatts)", 
-                main = "Global Active Power", cex.axis = 0.7))
+with(hpc_data, hist(Global_active_power, 
+                    col = "red", 
+                    xlab = "Global Active Power (kilowatts)", 
+                    main = "Global Active Power", cex.axis = 0.7))
 
 
 # Close the device and save the plot in the PNG image. 
--- plot3.R
+++ plot3.R
@@ -3,28 +3,38 @@
 #  Created by Adalberto Cubillo on 7/17/19.
 #  Copyright © 2019 Adalberto Cubillo. All rights reserved.
 
+# Download the household power consumption data zipped (the file is huge to store in github!).
+temp <- tempfile()
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", temp)
+downloaded_file <- unz(temp, "household_power_consumption.txt")
+
 # Read the household power consumption data. 
-data_header <- read.table(file = "./household_power_consumption.txt", 
+data_header <- read.table(file = downloaded_file, 
                           header = FALSE, sep = ";", nrows = 1, stringsAsFactors = FALSE)
-data <- read.table(file = "./household_power_consumption.txt", 
-                   header = FALSE, sep = ";", skip = 66637, nrows = 2881)
-colnames(data) <- as.vector(data_header[1,])
+
+# I need to load it again after the first read.
+downloaded_file <- unz(temp, "household_power_consumption.txt") 
+hpc_data <- read.table(file = downloaded_file,
+                       header = FALSE, sep = ";", skip = 66637, nrows = 2880)
+colnames(hpc_data) <- as.vector(data_header[1,])
+
+# Delete temp file.
+unlink(temp)
 
 # Convert Date and Time variables into R date and time types.
-# data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
-data$Time <- format(strptime(data$Time, format = "%H:%M:%S"), "%H:%M:%S")
-data$Date_Time <- strptime(paste(as.character(data$Date), as.character(data$Time)), 
-                           format = "%d/%m/%Y %H:%M:%S")
+hpc_data$Time <- format(strptime(hpc_data$Time, format = "%H:%M:%S"), "%H:%M:%S")
+hpc_data$Date_Time <- strptime(paste(as.character(hpc_data$Date), as.character(hpc_data$Time)), 
+                               format = "%d/%m/%Y %H:%M:%S")
 
 # Set the plot device to PNG and the size to 480x480px.
 png(filename = "./plot3.png", width = 480, height = 480, units = "px")
 
 # Create the plot3.
-with(data, plot(Date_Time, Sub_metering_1, type = "n", xlab = "",
-                ylab = "Energy sub metering"))
-with(data, points(Date_Time, Sub_metering_1, type = "l", col = "black"))
-with(data, points(Date_Time, Sub_metering_2, type = "l", col = "red"))
-with(data, points(Date_Time, Sub_metering_3, type = "l", col = "blue"))
+with(hpc_data, plot(Date_Time, Sub_metering_1, type = "n", xlab = "",
+                    ylab = "Energy sub metering"))
+with(hpc_data, points(Date_Time, Sub_metering_1, type = "l", col = "black"))
+with(hpc_data, points(Date_Time, Sub_metering_2, type = "l", col = "red"))
+with(hpc_data, points(Date_Time, Sub_metering_3, type = "l", col = "blue"))
 legend("topright", lty = 1, col = c("black", "red", "blue"), 
        legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
